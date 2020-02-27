e2adc63d39784736b226ac20c217f5b094f5c122
Dospacite <54844588+Dospacite@users.noreply.github.com>
2020-02-28 00:17:42 +0300

    Update omega.h

--- OmegaMain/include/omega.h
+++ OmegaMain/include/omega.h
@@ -24,7 +24,10 @@ class Movement {
   }
 
   /* function -> axisMovement
-  * 
+  * Powers the motors from the input it gets at the given "controller".
+  * @param controller Controller that the axis values will be taken from.
+  * @param MotorArray[] Motor array that includes the motors that will be powered. [Right, Left]
+  * @param threshold=5 The minimum absolute value that will power the motors. Reduces battery usage.
   */
   static void axisMovement(controller controller, motor MotorArray[], int threshold=5) {
     int powerLeft = (controller.Axis3.position() + controller.Axis4.position());
@@ -34,22 +37,20 @@ class Movement {
       MotorArray[1].spin(forward, powerRight, pct);
     }
   }
-
-  /* function -> spinForMS
-  *
-  */
-  static void spinForMS(motor MotorArray[2], uint16_t time, int power = 100, int motorCount = 2) {
-    MotorArray[0].spin(directionType::fwd, power, pct);
-    MotorArray[0].spin(directionType::fwd, power, pct);
-    task::sleep(time);
-    MotorArray[0].stop(brakeType::hold);
-    MotorArray[1].stop(brakeType::hold);
-  }
 };
 
 class Buttons {
+	
   public:
-  static void buttonFunction(controller::button button1, controller::button button2, motor MotorArray[], uint16_t power, uint16_t motorCount = 2) {
+  
+  /* function -> buttonFunction
+  * Powers the given motors when either button is pressed.
+  * @param button1 Button that will power the motors forward.
+  * @param button2 Button that will power the motors reverse.
+  * @param MotorArray[] Motors that will be powered when either buttons are pressed.
+  * @param power Power that the motors will be powered with.
+  */
+  static void buttonFunction(controller::button button1, controller::button button2, motor MotorArray[], uint16_t power) {
     if (button1.pressing()) {
       MotorArray[0].spin(directionType::fwd, power, velocityUnits::pct);
       MotorArray[1].spin(directionType::fwd, power, velocityUnits::pct);
@@ -63,7 +64,14 @@ class Buttons {
       MotorArray[1].stop(brakeType::hold);
     }
   }
-
+  /*
+  * Powers the given motors when either button is pressed. At the same time, powers the motors given with the LiftArray by a small amount.
+  * @param button1 Button that will power the motors forward.
+  * @param button2 Button that will power the motors reverse.
+  * @param LiftArray[] Motors that will be powered by a small amount when either buttons are pressed.
+  * @param MotorArray[] Motors that will be powered when either buttons are pressed.
+  * @param power Power that the motors in MotorArray will be powered with.
+  */
   static void armControl(controller::button button1, controller::button button2, motor LiftArray[2], motor MotorArray[2], int power) {
     if (button1.pressing()) {
       LiftArray[0].spin(forward, 10, pct);
